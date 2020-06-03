446aa404c8a390cfe5cee95648385bfb16a445c3
Corne Siebeling <2078538@talnet.nl>
2020-06-03 11:12:29 +0200

    taak 2 delete

--- 3-Delete/taak02/antwoorden.sql
+++ 3-Delete/taak02/antwoorden.sql
@@ -1,5 +1,11 @@
 -- Opdracht 3
-
+DELETE FROM players WHERE id = 198329
 -- Opdracht 4
-
+DELETE FROM players WHERE id = 198329
 -- Opdracht 5
+DELETE FROM players WHERE id=199304;
+DELETE FROM players WHERE id=207494;
+DELETE FROM players WHERE id=226753;
+
+ #1062 - Duplicate entry '198329' for key 'PRIMARY'
+ALTER TABLE `players` ADD PRIMARY KEY( `id`);
